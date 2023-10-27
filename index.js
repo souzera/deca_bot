97ba76380072b644c9690f58825115ba1cb6d374
unknown <towork.matheusb@gmail.com>
2023-10-27 14:31:43 -0300

    ping

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 import qrcode from 'qrcode-terminal'
 import { Client } from 'whatsapp-web.js'
 
-import { OpenAi } from 'openai'
 import dotenv from 'dotenv'
 
-const client = new Client() 
+const client = new Client()
 
 dotenv.config()
-const openai = new OpenAi({apiKey: process.env.OPENAI_API_KEY})
-
 const options = {
     apiKey: process.env.OPENAI_API_KEY,
-    organization: "org-G0B8EKLB1495Jc3Y5AmNlC15"
+    organization: process.env.OPENAI_ORGANIZATION_ID
 }
 
+// WHATSAPP - SIDE
+
 client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true})
-    console.log("Escaneie o QR Code acima para iniciar a sessão") 
+    qrcode.generate(qr, { small: true })
+    console.log("Escaneie o QR Code acima para iniciar a sessão")
     console.log('QR RECEIVED: ', qr)
-}) 
+})
 
 client.on('ready', () => {
     console.log('Client is ready!')
 })
 
-client.on('message', msg => {
+client.on('message', (msg)=> {
     console.log(msg.body)
-})
 
-client.on('message', message => {
-	if(message.body === '/eoq') {
-		message.reply('sei não');
-	}
-});
+    if (msg.body === '!ping') {
+        msg.reply(`🏓 Pong in poucos ms`)
+    }
+
+    if (msg.body === '!aniversario'){
+        msg.reply(`Feliz aniversário, ${msg.sender.pushname}! \n\n https://youtu.be/PMKrAU1IlnA`)
+    }
+})
 
 client.initialize();
