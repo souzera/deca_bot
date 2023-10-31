efd8dc82e86e5744db550c60370d31d74feacc02
unknown <towork.matheusb@gmail.com>
2023-10-31 13:32:59 -0300

    update api weather

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import qrcode from 'qrcode-terminal'
-import { Client } from 'whatsapp-web.js'
 
 import dotenv from 'dotenv'
 import axios from 'axios'
 
+import * as pkg from 'whatsapp-web.js'
+
+const { Client, MessageMedia } = pkg;
+
 dotenv.config()
+
+// HG API Weather
+
+const hg_api_key = process.env.HG_API_KEY
+
+const getWeather = async (city) => {
+
+    console.log(`Query city: ${city}`)
+
+    try {
+        let bot_response = ""
+        await axios.get(`https://api.hgbrasil.com/weather?key=${hg_api_key}&city_name=${city}`).then((response) => {
+            const temperature = response.data.results.temp
+            switch (temperature) {
+                case temperature < 15:
+                    bot_response += `🥶 Como está frio no Alaska. Fazem exatamente ${temperature} ℃`
+                    break
+                case temperature >= 15 && temperature < 28:
+                    bot_response += `😎 Suave na nave 🚀 só de boa. Fazem exatamente ${temperature} ℃`
+                    break
+                case temperature >= 28:
+                    bot_response += `🥵 Calor da febe homi, desliga o sol. Fazem exatamente ${temperature} ℃`
+                    break
+            }
+
+            return bot_response
+        })
+    } catch (err) {
+        console.log(err)
+        return "❌ Não foi possivel obter o clima."
+    }
+}
 
 // OPENAI
 
@@ -15,10 +50,10 @@ const options = {
 
 const getDavinciResponse = async (clientText) => {
     const davinci = {
-        model: "user", 
+        model: "user",
         prompt: clientText,
-        temperature: 1, 
-        max_tokens: 4000 
+        temperature: 1,
+        max_tokens: 4000
     }
 
     try {
@@ -29,7 +64,7 @@ const getDavinciResponse = async (clientText) => {
                 url: "https://api.openai.com/v1/chat/completions",
                 data: {
                     model: "gpt-3.5-turbo",
-                    messages: [{"role": "user","content": davinci.prompt},]
+                    messages: [{ "role": "user", "content": davinci.prompt },]
                 },
                 headers: {
                     "Content-Type": "application/json",
@@ -37,8 +72,6 @@ const getDavinciResponse = async (clientText) => {
                 }
             }
         ).then((response) => {
-            console.log("Resposta: ")
-            console.log(response.data.choices[0].message)
             botResponse += response.data.choices[0].message.content
         })
         return `Chat GPT 🤖\n\n ${botResponse.trim()}`
@@ -50,7 +83,7 @@ const getDavinciResponse = async (clientText) => {
 const getDalleResponse = async (clientText) => {
     const data = {
         prompt: clientText,
-        n: 1, 
+        n: 1,
         size: "1024x1024",
     }
 
@@ -65,9 +98,8 @@ const getDalleResponse = async (clientText) => {
                     "Authorization": `Bearer ${options.apiKey}`,
                 }
             }
-        ).then((response) =>{
-            console.log(response.data.data[0])
-            return response.data.data[0]
+        ).then((response) => {
+            return response.data.data[0].url
         })
     } catch (e) {
         return `❌ OpenAI Response Error: ${e.response.data.error.message}`
@@ -90,23 +122,31 @@ client.on('ready', () => {
 
 client.on('message', (msg) => {
 
-    const contato = (msg) => {
-        msg.getContact().then((result) => {
+    const contato = async () => {
+        await msg.getContact().then((result) => {
             return result
         })
     }
 
-    const chat = (msg) => {
-        msg.getChat().then((result) => {return result})
+    contato().then((result) => {
+        console.log(`Send by: ${result}`)
+    })
+
+    const chat = async () => {
+        await msg.getChat().then((result) => { return result })
     }
+
+    chat().then((result) => {
+        console.log(`Origin Chat: ${result}`)
+    })
 
     const bot_commands = {
         ping: "!ping",
         aniversario: "!aniversario",
         help: "!help",
         gpt: "!gpt",
-        dalle: '!dalle'
-
+        dalle: '!dalle',
+        clima: "!clima"
     }
 
     let firstWord = ((msg.body.includes(' ')) ? msg.body.substring(0, msg.body.indexOf(" ")) : msg.body);
@@ -115,14 +155,14 @@ client.on('message', (msg) => {
     console.log(msg.body)
 
     switch (firstWord) {
-        case '!ping':
+        case bot_commands.ping:
             msg.reply('🏓 Pong in poucos ms')
             break
-        case '!aniversario':
-            msg.reply(`Feliz aniversário, ${contato(msg).pushname}! \n\n`)
+        case bot_commands.aniversario:
+            msg.reply(`Feliz aniversário, ${contato.pushname}! \n\n`)
             break
-        case '!help':
-            msg.reply(`Comandos disponíveis: \n\n !ping \n !aniversario \n !help`)
+        case bot_commands.help:
+            msg.reply(`Comandos disponíveis: \n\n !ping \n !gpt SEU_TEXTO \n !help`)
             break
         case bot_commands.gpt:
             const question = msg.body.substring(msg.body.indexOf(" "));
@@ -132,10 +172,10 @@ client.on('message', (msg) => {
             })
             break
         case bot_commands.dalle:
-            const desc = msg.body.substring(msg.body.indexOf(" "));
-            getDalleResponse(desc).then((response) => {
-                msg.reply(`Imagem: ${response.url}`)
-            })
+            msg.reply('Ops! Comando desativado ❌')
+            break
+        case bot_commands.clima:
+            msg.reply('Ops! Comando desativado ❌')
             break
     }
 })
